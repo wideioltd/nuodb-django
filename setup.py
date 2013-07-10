22fb31f9256b3f669faace939fcf8d91a065179e
Luke <lbayas@gmail.com>
2013-07-10 14:25:30 -0400

    bump version, add MANIFEST.in

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='django_pynuodb',
-    version='0.2',
+    version='0.3',
     author='NuoDB',
     author_email='info@nuodb.com',
     description="NuoDB Extension for Django",
