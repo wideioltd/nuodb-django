49fa42169986b6a23827a266034abea14e545cf1
Dan Weiss <dweiss@nuodb.com>
2013-07-23 11:02:02 -0400

    fixed more entity problems

--- django_pynuodb/creation.py
+++ django_pynuodb/creation.py
@@ -290,10 +290,10 @@ class DatabaseCreation(BaseDatabaseCreation):
         try:
             test_database_name = self._get_test_db_name()
             if test_database_name not in [db.name for db in domain.databases]:
-                peer = domain.getEntryPeer()
+                peer = domain.entry_peer
                 archive = os.path.join(tempfile.gettempdir(), ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(20)))
-                peer.startStorageManager(test_database_name, archive, True, waitSeconds=10)
-                peer.startTransactionEngine(test_database_name,  [('--dba-user', DBA_USER),('--dba-password', DBA_PASSWORD)], waitSeconds=10)
+                peer.start_storage_manager(test_database_name, archive, True, wait_seconds=10)
+                peer.start_transaction_engine(test_database_name,  [('--dba-user', DBA_USER),('--dba-password', DBA_PASSWORD)], wait_seconds=10)
             return test_database_name
         finally:
             domain.disconnect()
