a0d0ddd33547fd874e67852a0e5d7d23836a7c3f
creyes <cer26@duke.edu>
2013-07-25 10:55:11 -0400

    added django tests startup script

---
+++ django_tests.sh
@@ -0,0 +1,27 @@
+sudo apt-get install python-pip
+sudo pip install django
+sudo pip install pynuodb
+sudo pip install django_pynuodb
+
+cd /tmp
+django-admin.py startproject nuodb_site
+
+cd /tmp/nuodb_site/nuodb_site
+sed -i "s/'ENGINE': 'django.db.backends.',/'ENGINE': 'django_pynuodb', 'NAME':'test',/" settings.py
+sed -i "s/'NAME': '',/'USER': 'dba',/" settings.py
+sed -i "s/'USER': '',/'SCHEMA': 'django',/" settings.py
+sed -i "s/'PASSWORD': '',/'PASSWORD': 'goalie',/" settings.py
+sed -i "s/'HOST': '',/'HOST': 'localhost',/" settings.py
+sed -i "s/'PORT': '',/'PORT': '48004',/" settings.py
+sed -i "s/USE_TZ = True/USE_TZ = False/" settings.py
+
+cd /tmp
+git clone https://github.com/nuodb/nuodb-django.git
+
+cd /opt/nuodb
+quit | ./run-quickstart
+
+cd /tmp/nuodb_site
+yes | python manage.py syncdb
+
+python manage.py test
