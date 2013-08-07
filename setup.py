cfb20d7243a7712596da0ebf91ae5261a759134c
Luke <lbayas@gmail.com>
2013-08-07 10:14:28 -0400

    bump version to 0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='django_pynuodb',
-    version='0.5',
+    version='0.6',
     author='NuoDB',
     author_email='info@nuodb.com',
     description="NuoDB Extension for Django",
