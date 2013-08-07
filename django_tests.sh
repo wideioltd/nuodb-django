c123e215a1a1a13d5fe7064f45272ee1e9834608
creyes <cer26@duke.edu>
2013-08-07 15:11:59 -0400

    sed fix

--- django_tests.sh
+++ django_tests.sh
@@ -1,16 +1,16 @@
 cd /tmp
 django-admin.py startproject nuodb_site
 cd /tmp/nuodb_site/nuodb_site
-sed -i "s/'ENGINE': 'django.db.backends.',/'ENGINE': 'django_pynuodb', 'NAME':'test',/" settings.py
+sed -i "s/'ENGINE': 'django.db.backends.',/'ENGINE': 'django_pynuodb',/" settings.py
 sed -i "s/'NAME': '',/'DBA_USER': 'dba',/" settings.py
 sed -i "s/'USER': '',/'SCHEMA': 'django',/" settings.py
 sed -i "s/'PASSWORD': '',/'DBA_PASSWORD': 'goalie',/" settings.py
 sed -i "s/'HOST': '',/'HOST': 'localhost',/" settings.py
 sed -i "s/'PORT': '',/'PORT': '48004',/" settings.py
 
-sed -i "s/'PORT': '48004',/\n    'NAME': 'test',/" settings.py
-sed -i "s/'PORT': '48004',/\n    'DOMAIN_USER': 'domain',/" settings.py
-sed -i "s/'PORT': '48004',/\n    'DOMAIN_PASSWORD': 'bird',/" settings.py
+sed -i "s/'PORT': '48004',/'PORT': '48004',\n    'NAME': 'test',/" settings.py
+sed -i "s/'PORT': '48004',/'PORT': '48004',\n    'DOMAIN_USER': 'domain',/" settings.py
+sed -i "s/'PORT': '48004',/'PORT': '48004',\n    'DOMAIN_PASSWORD': 'bird',/" settings.py
 
 
 sed -i "s/USE_TZ = True/USE_TZ = False/" settings.py
